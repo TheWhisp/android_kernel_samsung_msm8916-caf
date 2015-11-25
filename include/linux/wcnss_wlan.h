7ed65bca2c86c33fff9c212a2699c79882ff84b1
Padma, Santhosh Kumar <skpadma@codeaurora.org>
2015-11-25 12:30:19 +0530

    wcnss: Expose dump stack functionality
    Add changes to expose dump stack functionality which can be used
    by driver to dump stack information when it requires.
    
    CRs-Fixed: 943322
    Change-Id: I0fde7142dea2c18daf6b1fb0c5ee4bb8a31a6be0
    Signed-off-by: Padma, Santhosh Kumar <skpadma@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -15,6 +15,7 @@
 #define _WCNSS_WLAN_H_
 
 #include <linux/device.h>
+#include <linux/sched.h>
 
 enum wcnss_opcode {
 	WCNSS_WLAN_SWITCH_OFF = 0,
@@ -112,6 +113,7 @@ u32 wcnss_get_wlan_rx_buff_count(void);
 int wcnss_wlan_iris_xo_mode(void);
 void wcnss_flush_work(struct work_struct *work);
 void wcnss_flush_delayed_work(struct delayed_work *dwork);
+void wcnss_dump_stack(struct task_struct *task);
 
 #ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 void wcnss_log_debug_regs_on_bite(void);

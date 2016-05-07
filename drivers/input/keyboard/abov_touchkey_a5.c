56dfd20a3354ea1c89d5bf974af642e34c05ac87
Davis Malnieks <malnieks.davis@gmail.com>
2016-05-07 22:18:18 +0300

    touch_abov_a5: add new sysfs for disabling touckeys for inputdisabler

--- drivers/input/keyboard/abov_touchkey_a5.c
+++ drivers/input/keyboard/abov_touchkey_a5.c
@@ -140,7 +140,7 @@ struct abov_tk_info {
 	bool led_twinkle_check;
 #endif
 	bool dual_mode;
-
+	int force_disable;
 };
 
 
@@ -1219,6 +1219,29 @@ static ssize_t abov_set_dual_detection_mode(struct device *dev,
 	return count;
 }
 
+static ssize_t touckey_force_disable_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct abov_tk_info *info = dev_get_drvdata(dev);
+
+	return snprintf(buf, BUF_SIZE, "%u\n", info->force_disable);
+}
+
+static ssize_t touckey_force_disable_store(struct device *dev,
+				struct device_attribute *attr, const char *buf, size_t count)
+{
+	unsigned int force_disable;
+	struct abov_tk_info *info = dev_get_drvdata(dev);
+
+	if(sscanf(buf, "%u", &force_disable) != 1)
+		return -EINVAL;
+
+	if(force_disable > 1)
+		return -EINVAL;
+
+	info->force_disable = force_disable;
+
+	return count;
+}
 
 static DEVICE_ATTR(touchkey_threshold, S_IRUGO, touchkey_threshold_show, NULL);
 static DEVICE_ATTR(brightness, S_IRUGO | S_IWUSR | S_IWGRP, NULL,
@@ -1237,6 +1260,7 @@ static DEVICE_ATTR(glove_mode, S_IRUGO | S_IWUSR | S_IWGRP,
 			abov_glove_mode_show, abov_glove_mode);
 static DEVICE_ATTR(detection_mode, S_IRUGO | S_IWUSR | S_IWGRP,
 			NULL, abov_set_dual_detection_mode);
+static DEVICE_ATTR(force_disable, 0664, touckey_force_disable_show, touckey_force_disable_store);
 
 static struct attribute *sec_touchkey_attributes[] = {
 	&dev_attr_touchkey_threshold.attr,
@@ -1251,6 +1275,7 @@ static struct attribute *sec_touchkey_attributes[] = {
 	&dev_attr_touchkey_firm_update_status.attr,
 	&dev_attr_glove_mode.attr,
 	&dev_attr_detection_mode.attr,
+	&dev_attr_force_disable.attr,
 	NULL,
 };
 
@@ -1571,6 +1596,7 @@ static int abov_tk_probe(struct i2c_client *client,
 		info->pdata->power(info->pdata, true);
 	msleep(ABOV_RESET_DELAY);
 
+	info->enabled = true;
 	info->enabled = true;
 	info->irq = -1;
 	mutex_init(&info->lock);
